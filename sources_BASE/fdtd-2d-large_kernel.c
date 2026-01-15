d57f77b0308b29adc2e50c6cf31a9e4db8228137
ajh9498 <ajh9498@cepool-a00.poly.edu>
2026-01-15 14:45:28 -0500

    fdtd-2d-large annotated base

--- sources_BASE/fdtd-2d-large_kernel.c
+++ sources_BASE/fdtd-2d-large_kernel.c
@@ -6,44 +6,48 @@ void kernel_fdtd_2d(double ex[200][240], double ey[200][240],
     int j;
 
     L0: for (t = 0; t < 100; t++) {
-        // @slot __PIPE__L0
-        // @slot __UNROLL__L0
+    // @slot __PIPE__L0
+    // @slot __UNROLL__L0
 
         L1: for (j = 0; j < 240; j++) {
-            // @slot __UNROLL__L1
+        // @slot __PIPE__L1
+        // @slot __UNROLL__L1
             
             ey[0][j] = _fict_[t];
         }
 
         L2: for (i = 1; i < 200; i++) {
-            // @slot __PIPE__L2
-            // @slot __UNROLL__L2
+        // @slot __PIPE__L2
+        // @slot __UNROLL__L2
 
             L5: for (j = 0; j < 240; j++) {
-                // @slot __UNROLL__L5
+            // @slot __PIPE__L5
+            // @slot __UNROLL__L5
 
                 ey[i][j] = ey[i][j] - 0.5 * (hz[i][j] - hz[i - 1][j]);
             }
         }
 
         L3: for (i = 0; i < 200; i++) {
-            // @slot __PIPE__L3
-            // @slot __UNROLL__L3
+        // @slot __PIPE__L3
+        // @slot __UNROLL__L3
 
             L6: for (j = 1; j < 240; j++) {
-                // @slot __UNROLL__L6
+            // @slot __PIPE__L6
+            // @slot __UNROLL__L6
 
                 ex[i][j] = ex[i][j] - 0.5 * (hz[i][j] - hz[i][j - 1]);
             }
         }
 
         L4: for (i = 0; i < 200 - 1; i++) {
-            // @slot __PIPE__L4
-            // @slot __UNROLL__L4
+        // @slot __PIPE__L4
+        // @slot __UNROLL__L4
 
             L7: for (j = 0; j < 240 - 1; j++) {
-                // @slot __UNROLL__L7
-                
+            // @slot __PIPE__L7
+            // @slot __UNROLL__L7
+            
                 hz[i][j] = hz[i][j] - 0.7 * (ex[i][j + 1] - ex[i][j] + ey[i + 1][j] - ey[i][j]);
             }
         }
