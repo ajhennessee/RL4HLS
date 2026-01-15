9e592e57c48ab64da96c1f1a047112a54e6ac7b3
ajh9498 <ajh9498@cepool-a00.poly.edu>
2026-01-15 13:47:35 -0500

    2mm annotated base

--- sources_BASE/2mm_kernel.c
+++ sources_BASE/2mm_kernel.c
@@ -8,18 +8,18 @@ void kernel_2mm(double alpha, double beta, double tmp[40][50], double A[40][70],
     /* D := alpha*A*B*C + beta*D */
 
     L0: for (i = 0; i < 40; i++) {
-        // @slot __PIPE__L0
-        // @slot __UNROLL__L0
+    // @slot __PIPE__L0
+    // @slot __UNROLL__L0
 
         L2: for (j = 0; j < 50; j++) {
-            // @slot __PIPE__L2
-            // @slot __UNROLL__L2
+        // @slot __PIPE__L2
+        // @slot __UNROLL__L2
 
             tmp[i][j] = 0.0;
 
             L4: for (k = 0; k < 70; ++k) {
-                // @slot __PIPE__L4
-                // @slot __UNROLL__L4
+            // @slot __PIPE__L4
+            // @slot __UNROLL__L4
 
                 tmp[i][j] += alpha * A[i][k] * B[k][j];
             }
@@ -27,18 +27,18 @@ void kernel_2mm(double alpha, double beta, double tmp[40][50], double A[40][70],
     }
 
     L1: for (i = 0; i < 40; i++) {
-        // @slot __PIPE__L1
-        // @slot __UNROLL__L1
+    // @slot __PIPE__L1
+    // @slot __UNROLL__L1
 
         L3: for (j = 0; j < 80; j++) {
-            // @slot __PIPE__L3
-            // @slot __UNROLL__L3
+        // @slot __PIPE__L3
+        // @slot __UNROLL__L3
 
             D[i][j] *= beta;
 
             L5: for (k = 0; k < 50; ++k) {
-                // @slot __PIPE__L5
-                // @slot __UNROLL__L5
+            // @slot __PIPE__L5
+            // @slot __UNROLL__L5
 
                 D[i][j] += tmp[i][k] * C[k][j];
             }
