af89076ec538c1a658f0a3f6d15d9df13f85ac5a
ajh9498 <ajh9498@cepool-a00.poly.edu>
2026-01-15 14:01:12 -0500

    atax annotated base

--- sources_BASE/atax_kernel.c
+++ sources_BASE/atax_kernel.c
@@ -4,23 +4,25 @@ void kernel_atax(double A[116][124], double x[124], double y[124],
     int i;
     int j;
 
-    L0: for (i = 0; i < 124; i++)
+    for (i = 0; i < 124; i++)
         y[i] = ((double)0);
 
-    L1: for (i = 0; i < 116; i++) {
-        // @slot __PIPE__L1
-        // @slot __UNROLL__L1
+    L0: for (i = 0; i < 116; i++) {
+    // @slot __PIPE__L0
+    // @slot __UNROLL__L0
 
         tmp[i] = 0.0;
 
-        L2: for (j = 0; j < 124; j++) {
-            // @slot __UNROLL__L2
+        L1: for (j = 0; j < 124; j++) {
+        // @slot __PIPE__L1
+        // @slot __UNROLL__L1
 
             tmp[i] += A[i][j] * x[j];
         }
 
-        L3: for (j = 0; j < 124; j++) {
-            // @slot __UNROLL__L3
+        L2: for (j = 0; j < 124; j++) {
+        // @slot __PIPE__L2
+        // @slot __UNROLL__L2
 
             y[j] += A[i][j] * tmp[i];
         }
