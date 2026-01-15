6faf1e54e09572889e4a7cc9a4949b35802029d9
ajh9498 <ajh9498@cepool-a00.poly.edu>
2026-01-15 14:09:27 -0500

    bicg annotated base

--- sources_BASE/bicg_kernel.c
+++ sources_BASE/bicg_kernel.c
@@ -5,19 +5,21 @@ void kernel_bicg(double A[124][116], double s[116], double q[124],
     int j;
 
     L0: for (i = 0; i < 116; i++) {
-        // @slot __UNROLL__L0
+    // @slot __PIPE__L0
+    // @slot __UNROLL__L0
 
         s[i] = ((double)0);
     }
 
     L1: for (i = 0; i < 124; i++) {
-        // @slot __PIPE__L1
-        // @slot __UNROLL__L1
+    // @slot __PIPE__L1
+    // @slot __UNROLL__L1
 
         q[i] = 0.0;
 
         L2: for (j = 0; j < 116; j++) {
-            // @slot __UNROLL__L2
+        // @slot __PIPE__L2
+        // @slot __UNROLL__L2
 
             s[j] += r[i] * A[i][j];
             q[i] += A[i][j] * p[j];
